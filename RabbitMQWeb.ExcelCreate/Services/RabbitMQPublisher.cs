412141b0b795aeb171481e81b513667fee1932a9
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-08-18 15:32:31 +0300

    Add RabbitMQ publisher service

--- RabbitMQWeb.ExcelCreate/Services/RabbitMQPublisher.cs
+++ RabbitMQWeb.ExcelCreate/Services/RabbitMQPublisher.cs
@@ -1,18 +1,34 @@
-﻿namespace RabbitMQWeb.ExcelCreate.Services
+﻿using RabbitMQ.Client;
+using RabbitMQWeb.ExcelCreate.Models;
+using System.Text;
+using System.Text.Json;
+
+namespace RabbitMQWeb.ExcelCreate.Services
 {
     public class RabbitMQPublisher
     {
         private readonly RabbitMQClientService _rabbitmqClientService;
 
-        public RabbitMQPublisher(RabbitMQClientService  )
+        public RabbitMQPublisher(RabbitMQClientService rabbitMQClientService)
+
         {
-            
+            _rabbitmqClientService = rabbitMQClientService;
         }
 
+        public void Publish(CreateExcelMessage productImageCreatedEvent)
+        {
+            var channel = _rabbitmqClientService.Connect();
 
+            var bodyString = JsonSerializer.Serialize(productImageCreatedEvent);
 
+            var bodyByte = Encoding.UTF8.GetBytes(bodyString);
 
+            var properties = channel.CreateBasicProperties();
+            properties.Persistent = true;
 
-
+            channel.BasicPublish(exchange: RabbitMQClientService.ExchangeName,
+                routingKey: RabbitMQClientService.RoutingExcel, basicProperties:
+                properties, body: bodyByte);
+        }
     }
 }
